3fac17e2f4132905c27cfc28c5854ea4c27e5f0e
Hephaestus <zeyinlu@Hephaestus.local>
2022-04-09 14:30:07 -0400

    Updating gameflow

--- scripts/components/quizTally.py
+++ scripts/components/quizTally.py
@@ -7,7 +7,7 @@ def total(value):
     if value == 40:
         vars.character = vars.characters[0]
 
-        print("It's " + vars.character)
+        print("You are " + vars.character)
        
         myImage = Image.open("bruce.jpg")
         myImage.show()
@@ -16,7 +16,7 @@ def total(value):
     elif value == 130:
         vars.character = vars.characters[1]
 
-        print("It's " + vars.character)
+        print("You are " + vars.character)
 
         myImage = Image.open("thor.jpg")
         myImage.show()
@@ -25,7 +25,7 @@ def total(value):
     elif value == 260:
         vars.character = vars.characters[2]
 
-        print("It's " + vars.character)
+        print("You are " + vars.character)
         myImage = Image.open("natasha.jpg")
         myImage.show()
         # add some emoji icons, or show the character image using the Pillow package
@@ -33,7 +33,7 @@ def total(value):
     elif value == 80:
         vars.character = vars.characters[3]
 
-        print("It's " + vars.character)
+        print("You are " + vars.character)
         myImage = Image.open("steve.jpg")
         myImage.show()
         # add some emoji icons, or show the character image using the Pillow package
@@ -41,7 +41,7 @@ def total(value):
     elif value == 60:
         vars.character = vars.characters[4]
 
-        print("It's " + vars.character)
+        print("You are " + vars.character)
         myImage = Image.open("tony.jpg")
         myImage.show()
         # add some emoji icons, or show the character image using the Pillow package
@@ -49,7 +49,7 @@ def total(value):
     elif value == 100:
         vars.character = vars.characters[5]
 
-        print("It's " + vars.character)
+        print("You are " + vars.character)
         myImage = Image.open("clint.jpg")
         myImage.show()
         # add some emoji icons, or show the character image using the Pillow package
